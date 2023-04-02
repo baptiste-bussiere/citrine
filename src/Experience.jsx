be010d4a94057c3cfe338877660dda3b82ec9b81
Baptiste <70062660+babat-10@users.noreply.github.com>
2023-04-02 20:48:15 +0200

    add iframe

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,84 +1,107 @@
-import { Text, Html, ContactShadows,PresentationControls, SpotLight, Environment, useGLTF, OrbitControls, Plane } from '@react-three/drei'
-import { useControls } from 'leva'
-import React, { useRef } from 'react';
+import {
+  Text,
+  Html,
+  ContactShadows,
+  PresentationControls,
+  Float,
+  OrbitControls,
+  SoftShadows,
+  Environment,
+  useHelper,
+  useGLTF,
+  Shadow,
+  Plane,
+} from "@react-three/drei";
+import React, { useRef, useState } from "react";
+import { useControls } from "leva";
+import { RectAreaLightHelper } from "three/addons/helpers/RectAreaLightHelper.js";
 
+export default function Experience() {
+  const computer = useGLTF("/public/model/model.glb");
+  const spotlightRef = useRef();
+  const [hidden, set] = useState();
+  console.log(OrbitControls);
 
-export default function Experience()
+  useHelper(spotlightRef, RectAreaLightHelper, "cyan");
+  const { position, rotation } = useControls({
+    position: {
+      value: { x: 0, y: 0, z: 0 },
+      step: 0.01,
+    },
+    rotation: {
+      value: { x: 0, y: 0, z: 0 },
+      step: 0.01,
+    },
+    color: {
+      r: 255,
+      g: 255,
+      b: 255,
+    },
+  });
 
-{
-  
-    const computer = useGLTF('/public/model/model.glb')
-    const spotlightRef = useRef();
+  return (
+    <>
+      <color args={["#000"]} attach="background" />
+      <spotLight
+        color={"#ffdebb"}
+        position={[0, 10, 0]}
+        penumbra={1}
+        distance={200}
+        angle={1.9}
+        attenuation={1}
+        intensity={1.2}
+        decay={0.1}
+      />
 
-    const spotlightControls = useControls('sphere', {
-      myNumber: { value: 0, step: 1 },
+      <rectAreaLight
+        width={1.8}
+        height={1.5}
+        intensity={20}
+        color={"#0000FF"}
+        position={[-2.4, 3.99, 3.4]}
+        rotation={[0.07, -0.72, 0.05]}
+      />
+      <rectAreaLight
+        width={0.2}
+        height={0.2}
+        position={[-3.47, 4.95, 1.33]}
+        rotation={[-1.53, -0.05, 0]}
+        intensity={2000}
+        color={"#ff7b00"}
+      />
+      <Plane
+        rotation={[-Math.PI / 2, 0, 0]}
+        position={[0, -1.3, 0]}
+        args={[1000, 1000]}
+      >
+        <meshStandardMaterial attach="material" color="black" />
+      </Plane>
 
-      position: {
-        x: 0,
-        y: 0,
-        z: 0,
-      },
-      rotation: {
-        x: 0,
-        y: 0,
-        z: 0,
-      },
-      color: {
-        r: 255,
-        g: 255,
-        b: 255,
-      },}
-    )
-    return <>
+      <OrbitControls
+        enablePan={false}
+        minAzimuthAngle={Math.PI / 2}
+        maxAzimuthAngle={-Math.PI}
+        minPolarAngle={-Math.PI / 4}
+        maxPolarAngle={Math.PI / 2}
+        minDistance={10}
+        maxDistance={30}
+      />
 
-        <color args={ [ '#000' ] } attach="background" />
+      <primitive object={computer.scene} position-y={-1.2}></primitive>
+      <Html
+        ref={spotlightRef}
+        transform
+        wrapperClass="htmlScreen"
+        distanceFactor={1}
+        position={[-2.4, 3.92, 3.14]}
+        rotation={[0.13, -3.87, -0.09]}
+        padding={2}
+        occlude="blending"
+      >
+        <iframe src="http://baptiste-bussiere.fr/bs.png" />
+      </Html>
 
-        <SpotLight
-          ref={spotlightRef}
-          rotate={[            
-            spotlightControls.rotation.x,
-            spotlightControls.rotation.y,
-            spotlightControls.rotation.z,
-
-          ]}
-          penumbra={1}
-          position={[
-            spotlightControls.position.x,
-            spotlightControls.position.y,
-            spotlightControls.position.z,
-          ]}
-          distance={30}
-          angle={2}
-          attenuation={0}
-          anglePower={10} 
-          color={`rgb(${spotlightControls.color.r}, ${spotlightControls.color.g}, ${spotlightControls.color.b})`}
-        />
-
-        <OrbitControls />
-                {/* <rectAreaLight
-                    width={ 2.5 }
-                    height={ 1.65 }
-                    intensity={ 65 }
-                    color={ '#ff6900' }
-                    rotation={ [ - 0.1, Math.PI, 0 ] }
-                    position={ [ 0, 0.55, - 1.15 ] }
-                /> */}
-
-                <primitive
-                    object={ computer.scene }
-                   scale={ [ 0.5, 0.5, 0.5 ] }
-                    // rotation-x={ 0.13 }
-                >
-                    <Html
-                        transform
-                        wrapperClass="htmlScreen"
-                        distanceFactor={ 1.17 }
-                        position={ [ -2, 5.1, 4 ] }
-                        rotation={[0, 2.4 ,0]}
-                        side={ 0 }
-                    >
-                        <iframe src="https://bruno-simon.com/html/" />
-                    </Html>
-                </primitive>
+      <ContactShadows position-y={-1.2} opacity={1} scale={20} blur={0.8} />
     </>
+  );
 }
